053d69ec0452073e824fbf3a521a812f8fc1e132
yan <15316488+fish-yan@users.noreply.github.com>
2024-10-09 11:20:24 +0800

    fix：6963 伪装metamask

--- packages/ios-web3-provider/swift/trust-min.js
+++ packages/ios-web3-provider/swift/trust-min.js
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:4efd04be73a34f7bb84287b66f9f1f61284f6916d1b3d14cc589c26bce8ab0ce
-size 439184
+oid sha256:954bb178ed45b28985ca996715a603ddec292fe307c18a7ff665dbe30eeec4c9
+size 443868
