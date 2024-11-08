a81f5b047e83f9ee584d7a483907dced768de252
chenyoh <chenyonghui@onchain.com>
2024-11-08 10:56:51 +0800

    fix: update trust-min.js

--- packages/ios-web3-provider/swift/trust-min.js
+++ packages/ios-web3-provider/swift/trust-min.js
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:458958447e6869f0ceaee25300f512721c52deb729e7a9451fcb75f9d5257b49
-size 1217388
+oid sha256:446eb9f000e1608a982de13acb4cabad36367dc2d30456e1b72346faa6858ac8
+size 1217361
